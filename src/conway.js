0eb36ebf5b40ec3ec472381d799ea336bac962f8
Chris Wagner <eelhovercraft@gmail.com>
2014-05-21 19:31:59 -0400

    Stubbed initial grid + first test

---
+++ src/conway.js
@@ -0,0 +1,17 @@
+function Cell() {
+    this.isAlive = false;
+}
+
+function Grid() {
+    var i, j;
+    this.rows = new Array(100);
+    for (i = 0; i < 100; ++i) {
+        this.rows[i] = new Array(100);
+        for (j = 0; j < 100; ++j) {
+            this.rows[i][j] = new Cell();
+        }
+    }
+}
+
+module.exports.Grid = Grid;
+module.exports.Cell = Cell;
