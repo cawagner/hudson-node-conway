e4fc09fb26bf18b22a6b5cc8b09bc913e6077398
Chris Wagner <eelhovercraft@gmail.com>
2014-05-21 19:33:13 -0400

    Added next test.

--- src/test/tests.js
+++ src/test/tests.js
@@ -7,5 +7,10 @@ describe('A grid', function(){
     it('should have 100 rows', function() {
         assert.equal(100, grid.rows.length);
     });
+    it('should have 100 cells per row', function() {
+        grid.rows.forEach(function(row) {
+            assert.equal(100, row.length);
+        });
+    });
   })
 });
